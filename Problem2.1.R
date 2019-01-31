d2e5602bc89fea38e028b2963a0936262f229a30
Nukeguy5 <nukeguy5@gmail.com>
2019-01-30 22:26:01 -0800

    Updated 2.1 Problem

--- Problem2.1.R
+++ Problem2.1.R
@@ -1,6 +1,12 @@
 load("college_scorecard_ca.Rda")
-na_pub <- is.na(college_scorecard_ca_latest$npt4_pub)
-na_priv <- is.na(college_scorecard_ca_latest$npt4_priv)
-count_pub <- sum(na_pub)
-count_priv <- sum(na_priv)
-count <- count_pub + count_priv
+# na_pub <- is.na(college_scorecard_ca_latest$npt4_pub)
+# na_priv <- is.na(college_scorecard_ca_latest$npt4_priv)
+# count_pub <- sum(na_pub)
+# count_priv <- sum(na_priv)
+# count <- count_pub + count_priv
+
+non_na_colleges <- college_scorecard_ca_latest$instnm[!is.na(college_scorecard_ca_latest$instnm)]
+
+for(i in seq_along(non_na_colleges)){
+  apply(array, margin, ...)
+}
