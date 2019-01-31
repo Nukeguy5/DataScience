ece117f51f5a2b76803cee1d27f9f523d8c9abf8
Nukeguy5 <nukeguy5@gmail.com>
2019-01-31 09:14:31 -0800

    Updated HW 2

--- Problem2.2.R
+++ Problem2.2.R
@@ -1,3 +1,21 @@
 load("college_scorecard_ca.Rda")
 
-non_na_colleges <- filter(college_scorecard_ca_latest, !is.na(college_scorecard_ca_latest$instnm))
+df <- college_scorecard_ca_latest
+
+df$npt <- df$npt4_pub
+
+# Using complete.cases
+df_complete <- df[, c("instnm", "md_earn_wne_p10", "npt")]
+df_complete <- df_complete[complete.cases(df_complete),]
+summary(df_complete$md_earn_wne_p10)
+
+# Using filter
+library("dplyr")
+df_filter <- df %>%
+  filter(!is.na(npt)) %>%
+  filter(!is.na(md_earn_wne_p10))
+
+# Filter WJU with Z-Score
+wju <- df %>%
+  filter(instnm == "William Jessup University")
+wju$md_earn_wne_p10
