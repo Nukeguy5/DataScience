989faed2d897b8df6103de0cb30b106dd9d5bd94
Nukeguy5 <nukeguy5@gmail.com>
2019-02-13 11:01:55 -0800

    Updated Project 1

--- Project 1 (Game - Settlers of Catan)/Dice Rolls.R
+++ Project 1 (Game - Settlers of Catan)/Dice Rolls.R
@@ -3,33 +3,33 @@
 
 # Settlers of Catan
 
-dice <- 1:6
-
 # probability <- outer(dice, dice, '+')
 
 player1 <- data.frame(
-  placement <- table(c(8,5,10, 4,8,10))
+  placement <- c(8,5,10, 4,8,10)
 )
 
 player2 <- data.frame(
-  placement <- table(c(6,9,3, 5,11,4))
+  placement <- c(6,9,3, 5,11,4)
 )
 
-
 play_game <- function(rolls, player1, player2){
-  rolls <- sample(dice, rolls, replace = TRUE) + sample(dice, rolls, replace = TRUE)
-  for (roll in rolls){
-    if (roll %in% player1$Var1){
-      i <- which(player1$Var1 == roll)
-      cards_recvd <- player1$Freq[i]
-      player1$resource_cards <- player1$resource_cards + cards_recvd
-    }
-    if (roll %in% player2$Var1){
-      i <- which(player2$Var1 == roll)
-      cards_recvd <- player2$Freq[i]
-      player2$resource_cards <- player2$resource_cards + cards_recvd
-    }
+  dice <- c(1:6)
+  all_rolls <- sample(dice, rolls, replace = TRUE) + sample(dice, rolls, replace = TRUE)
+  cards_recv1 <- sum(player1$placement[player1$placement %in% all_rolls])
+  cards_recv2 <- sum(player2$placement[player2$placement %in% all_rolls])
+  print(paste("Player 1 num of cards: ", cards_recv1))
+  print(paste("Player 2 num of cards: ", cards_recv2))
+  result <- c(cards_recv1, cards_recv2)
+  return(result)
+}
+
+play_games <- function(n_games, n_rolls, player1, player2){
+  for (i in 1:n_games){
+    play_game(n_rolls, player1, player2)
   }
 }
 
-play_game(20, player1, player2)
+# play_game(40, player1, player2)
+
+play_games(100, 40, player1 = player1, player2 = player2)
